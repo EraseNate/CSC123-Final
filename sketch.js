d05d5470b8c6bd99cda271c2cecfb1ee13e98d31
EraseNate <ruthlessjr@outlook.com>
2024-11-19 13:41:22 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,40 +1,54 @@
 let title, scene, game, start, instruction, w, h, medievalFont;
 
-function preload() 
+function preload()
   {
-    medievalFont = loadFont('CSS Fonts/MedievalSharp-Book.ttf'); 
+    medievalFont = loadFont("CSS Fonts/MedievalSharp-Book.ttf");
   }
-function setup() {
-  createCanvas(800, 800);
-  
-  w = width;
-  h = height;
-  
-  title = new Title();
-  start = new playButton();
-  scene = new ShowMap();
-  game = new Game();
-  instruction = new instButton();
-}
 
-function draw() {
-  background(225);
-  title.display();
-  if (instruction.instActive)
-    {
-      instruction.exitDisplay()
-    }
-  else if (start.gameActive)
+function setup()
   {
-    if (scene.sceneActive) {
-        scene.display(); // Display map scene if active
-      } else {
-        game.display(); // Display battle scene if collision occurred
+    createCanvas(800, 800);
+    
+    w = width;
+    h = height;
+
+    title = new Title();
+    start = new playButton();
+    scene = new ShowMap();
+    game = new Game();
+    instruction = new instButton();
+  }
+
+function draw()
+  {
+    background(225);
+    if (!start.gameActive && !instruction.instActive)
+      {
+        title.isVisible = true;
+      }
+    else
+      {
+        title.isVisible = false;
+      }
+    title.display();
+    if (instruction.instActive)
+      {
+        instruction.exitDisplay();
+      }
+    else if (start.gameActive)
+      {
+        if (scene.sceneActive)
+          {
+            scene.display(); // Display map scene if active
+          }
+        else
+          {
+            game.display(); // Display battle scene if collision occurred
+          }
+      }
+    else
+      {
+        instruction.checkCollision();
+        start.checkCollision();
       }
   }
-  else
-  {
-    instruction.checkCollision();
-    start.checkCollision();
-  }
-}
